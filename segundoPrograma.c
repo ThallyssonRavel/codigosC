193f4dae80eb0850c20dd2424530231172946aee
Thallysson Ravel <74942957+ThallyssonRavel@users.noreply.github.com>
2023-04-03 15:48:25 -0300

    Segundo Programa
    colocando informações já pre definidas na tela do usuário.

---
+++ segundoPrograma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int minha_idade=21;
+    int ano_nascimento=2002;
+    int dia_nascimento=10;
+    int mes_nascimento=02;
+    int dia_aniversario_namoro=14;
+
+    printf("A minha idade e %i\n", minha_idade);
+    printf("O ano em que nasci e %i\n", ano_nascimento);
+    printf("O dia do meu nascimento e %i\n", dia_nascimento);
+    printf("O mes do meu nascimento e %i\n", mes_nascimento);
+    printf("O meu aniversario de namoro e no dia %i\n",dia_aniversario_namoro);
+
+
+    return ;
+}
