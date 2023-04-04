7494ee254ef37343182fb262acfb04407971fc64
Thallysson Ravel <74942957+ThallyssonRavel@users.noreply.github.com>
2023-04-04 12:49:59 -0300

    Terceiro Programa
    Informações do segundo código, só que dispostos em uma única linha.

---
+++ terceiroPrograma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int minha_idade=21;
+    int ano_nascimento=2002;
+    int dia_nascimento=10;
+    int mes_nascimento=02;
+    int dia_aniversario_namoro=14;
+
+    printf("A minha idade e %i\nO ano de nascimento e %i\nO dia de nascimento e %i\nO mes de nascimento e %i\nO dia de aniversario de namoro e %i\n",minha_idade,ano_nascimento,
+           dia_nascimento,mes_nascimento,dia_aniversario_namoro);
+
+
+
+
+
+    return 0;
+}
